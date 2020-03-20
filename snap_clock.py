840cc848c492c1906569c259fb274538f41883a0
Janusz S. Kulpa (@nsg-work) <kulpaj.dev@gmail.com>
2020-03-20 02:06:55 +0000

    fixing the get/set snap clock

--- snap_clock.py
+++ snap_clock.py
@@ -6,15 +6,67 @@ This program will query the SNAP frequency generator for the
 output frequency and amplitude.
 """
 
-import os
-
 import telnetlib
-import snap_redis
+from optparse import OptionParser
+import sys
 
-_host = "10.1.49.160"
+_host = "10.10.1.197"
 _port = 5025
 
-def get_clock_settings(store_in_redis=False):
+defaultF = 1800
+defaultA = 16
+
+
+def main():
+    parser = OptionParser(usage= 'Usage %prog options',
+        description='display or change the values of SNAP clock generator')
+
+    parser.add_option('-p', '--print', dest='do_print', action="store_true", default=False,
+            help ="Query and print current values")
+    parser.add_option('-f','--freq', dest='freq', type=float, default=None,
+            help ='set clock value in MHz')
+    parser.add_option('-a','--amp', dest='amp', type=float, default=None,
+            help ='set amplitude')
+
+    (options,args) = parser.parse_args()
+
+    
+
+    if len(sys.argv) <= 1:
+        print("no options provided")
+        parser.print_help()
+        sys.exit(1)
+
+    do_p = options.do_print;
+    freq = options.freq
+    amp = options.amp
+    if (do_p and (freq or amp)):
+        print('if -p option is provided, -a and -f option should not be given')
+        sys.exit(1)
+
+    if (do_p):
+        settings = get_clock_settings()
+        print("{}:{} {} MHz  amp {}".format(_host,_port,settings['freq']/1e6,settings['ampr']))
+    else:
+        if (freq):
+            set_freq(freq)
+        if (amp):
+            set_amp(amp)
+        print("done")
+
+def set_freq(freq):
+    telnet = telnetlib.Telnet()
+    telnet.open(_host, _port)
+
+    telnet.write("FREQ {} MHz\n".format(freq))
+
+def set_amp(amp):
+    telnet = telnetlib.Telnet()
+    telnet.open(_host, _port)
+
+    telnet.write("AMPR {} MHz\n".format(amp))
+
+def get_clock_settings():
 
 
     telnet = telnetlib.Telnet()
@@ -26,9 +78,6 @@ def get_clock_settings(store_in_redis=False):
     ampr = telnet.read_until("\n", 5)
 
     answer = { "type" : "clock", "freq" : float(freq), "ampr" : float(ampr) }
-
-    if(store_in_redis == True):
-        snap_redis.RedisManager.get_instance().set("snap_clock", answer)
 
     return answer
 
@@ -42,12 +91,5 @@ def set_ampr(ampr_db):
 
 
 if __name__== "__main__":
-
-    get_clock_settings(True)
-    get_clock_settings(False)
-    set_clock_freq(1800)
-    set_ampr(16.0)
-
-    print (snap_redis.RedisManager.get_instance().get("snap_clock"))
-
+    main()
 
